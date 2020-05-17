106e8ed860bb93d9524db42aa8558ef25c3a0f0e
icha <predrika@multiinti.com>
2020-05-17 22:34:44 +0700

    ongoing, booking detail

--- app/src/main/java/com/junior/autoshop/OnGoingDetailFragment.java
+++ app/src/main/java/com/junior/autoshop/OnGoingDetailFragment.java
@@ -9,18 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class ProfileFragment extends Fragment {
+public class OnGoingDetailFragment extends Fragment {
 
-    public ProfileFragment() {
-
+    public OnGoingDetailFragment() {
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_on_going_detail, container, false);
     }
-
-
 }
--- app/src/main/java/com/junior/autoshop/OnGoingFragment.java
+++ app/src/main/java/com/junior/autoshop/OnGoingFragment.java
@@ -9,20 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class OnGoingFragment extends Fragment {
 
     public OnGoingFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_on_going, container, false);
     }
 }
